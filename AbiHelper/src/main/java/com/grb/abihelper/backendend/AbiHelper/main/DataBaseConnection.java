be09062dabaa353cab458571b9537a55f88df8a1
Julian Bivins <julianbivins@MacBook-Air-von-Julian.local>
2024-07-10 10:53:24 +0200

    tried to fix DataBaseConnection on local device, will push to try on local server

--- AbiHelper/src/main/java/com/grb/abihelper/backendend/AbiHelper/main/DataBaseConnection.java
+++ AbiHelper/src/main/java/com/grb/abihelper/backendend/AbiHelper/main/DataBaseConnection.java
@@ -1,55 +1,20 @@
 package com.grb.abihelper.backendend.AbiHelper.main;
 
-/*
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DataBaseConnection {
 
-    public static int MYSQL_DB_NOT_FOUND = 1049;
-    /*private void function() {
+    public static void main(String[] args) {
+
         var dataSource = new MysqlDataSource();
         dataSource.setServerName("localhost");
         dataSource.setPort(3306);
-//        dataSource.setUser(System.getenv("MYSQLUSER"));
-//        dataSource.setPassword(System.getenv("MYSQLPASS"));
-
-
         try (Connection connection = dataSource.getConnection()) {
             System.out.println("connection.isValid(0) = " + connection.isValid(0));
-
-            // CRUD
-
-            // select
-
-            PreparedStatement ps = connection.prepareStatement("select * from USERS where name = ?");
-            ps.setString(1, "Marco");
-
-            ResultSet resultSet = ps.executeQuery();
-            while (resultSet.next()) {
-                System.out.println(resultSet.getInt("id") + " - " + resultSet.getString("name"));
-            }
-
-            // inserts
-
-            PreparedStatement insertPs = connection.prepareStatement("insert into USERS (name) values (?)");
-            insertPs.setString(1, "John");
-            int insertCount = insertPs.executeUpdate();
-            System.out.println("insertCount = " + insertCount);
-
-            // updates
-
-            PreparedStatement updatePs = connection.prepareStatement("update USERS set name = ? where name = ?");
-            updatePs.setString(1, "Johnny");
-            updatePs.setString(2, "John");
-            int updateCount = updatePs.executeUpdate();
-            System.out.println("updateCount = " + updateCount);
-
-            // deletes
-
-            PreparedStatement deletePs = connection.prepareStatement("delete from USERS where name = ?");
-            deletePs.setString(1, "Johnny");
-            int deleteCount = deletePs.executeUpdate();
-            System.out.println("deleteCount = " + deleteCount);
-
-
         } catch (
                 SQLException e) {
             e.printStackTrace();
@@ -57,4 +22,3 @@ public class DataBaseConnection {
     }
 }
 
-*/
