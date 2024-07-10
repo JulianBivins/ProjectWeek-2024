d10261bf966a818fd23eb6d460f6a510f856055e
TheFatTurtle <paulobonifacio2005@gmail.com>
2024-07-10 14:54:35 +0200

    Lösche DataBaseConnection.java, weil das ganze eh gleich nochmal in ein Service gepackt wird und es sonst nicht kompilieren würde.
    Funktioniert bisher ohne crash auf eigenen Server, können die Konfiguration die ich Stand jetzt auf dem Server habe in unter 30 Minuten auf den Linux Server packen.

--- AbiHelper/src/main/java/com/grb/abihelper/backendend/AbiHelper/main/AbiHelperApplication.java
+++ AbiHelper/src/main/java/com/grb/abihelper/backendend/AbiHelper/main/AbiHelperApplication.java
@@ -1,13 +1,33 @@
 package com.grb.abihelper.backendend.AbiHelper.main;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootApplication
 public class AbiHelperApplication {
 
 	public static void main(String[] args) {
+	try {
+
+		DataSource source = createDataSource();
+		Connection connection = source.getConnection();
+
 		SpringApplication.run(AbiHelperApplication.class, args);
+	}
+	catch (Exception ex) {
+		ex.printStackTrace();
+	}
+	}
+	public  static DataSource createDataSource() {
+		HikariDataSource source = new HikariDataSource();
+		source.setJdbcUrl("jdbc:mysql://localhost:3306/Abidatenbank");
+		source.setUsername("Bootspring");
+
+		return  source;
 	}
 
 }
