ea1878652b37f9db4b4c3e351fcc4142b20a66b0
Julian Bivins <julianbivins@MacBook-Air-von-Julian.local>
2024-07-11 11:50:19 +0200

    Filelist fundd

--- Front-End/js/table.js
+++ Front-End/js/table.js
@@ -13,3 +13,46 @@ document.getElementById('tableSearch').addEventListener('input', function () {
     }
   });
 });
+
+
+// Tabelle Dynamik entries
+const jsonData = [
+  {
+    "teil": "a",
+    "option": "1",
+    "jahr": "2025",
+    "kursArt": "GK",
+    "fach": "Java",
+    "uuid": "01223032",
+    "lösung": "false"
+  },
+  {
+    "teil": "b",
+    "option": "2",
+    "jahr": "2024",
+    "kursArt": "LK",
+    "fach": "Python",
+    "lösung": "true"
+  }
+]; 
+function generateTable(data) {
+  const tableBody = document.getElementById('table-body');
+
+  data.forEach(item => {
+    const tr = document.createElement('tr');
+
+    tr.innerHTML = `
+      <td>${item.jahr}</td>
+      <td>${item.teil}${item.option}</td>
+      <td>${item.fach}</td>
+      <td>${item.kursArt}</td>
+      <td>${item.lösung}</td>
+      <td><a href="#">Download</a></td>
+    `;
+
+    tableBody.appendChild(tr);
+  });
+}
+
+// Generate table on page load
+document.addEventListener('DOMContentLoaded', () => generateTable(jsonData));
